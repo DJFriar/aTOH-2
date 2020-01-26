f42989850441999ffaf59333bfb867d2dcfb9d54
DJFriar <me@tommyc.net>
2020-01-26 13:54:07 -0600

    initial commit

---
+++ app/src/main/java/net/tommyc/tourofhonor2/MainActivity.java
@@ -0,0 +1,15 @@
+package net.tommyc.tourofhonor2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
